3c16bc7d104ff3875a747696e2f09cd474131069
otrachea <xtrachea@gmail.com>
2022-06-09 15:06:54 -0400

    Install tailwind

--- src/App/Header.tsx
+++ src/App/Header.tsx
@@ -10,52 +10,67 @@ export default function Header(props: any) {
   const { toggleThemeMode } = props;
 
   return (
-    <Navbar expand="sm" fixed="top">
-      <Container fluid>
-        <Nav.Link onClick={() => scroll.scrollToTop()}>
-          <Brand>F. Ho</Brand>
-        </Nav.Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto align-items-center">
-            <Toggle className="me-auto" toggleThemeMode={toggleThemeMode} />
-            <Nav.Link
-              onClick={() => {
-                scroller.scrollTo("featured-works", {
-                  duration: 800,
-                  delay: 0,
-                  smooth: "easeInOutQuart",
-                  offset: -150,
-                });
-              }}
-            >
-              <Text>Work</Text>
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                scroller.scrollTo("about", {
-                  duration: 800,
-                  delay: 0,
-                  smooth: "easeInOutQuart",
-                  offset: -150,
-                });
-              }}
-            >
-              <Text>About</Text>
-            </Nav.Link>
-            <Nav.Link onClick={() => {
-                scroller.scrollTo("contact", {
-                  duration: 800,
-                  delay: 0,
-                  smooth: "easeInOutQuart",
-                  offset: -150,
-                });
-              }}>
-              <Text>Contact</Text>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header className="text-white">
+      <div className="text-orange-200 bold">
+        F. Ho
+      </div>
+      <div>
+        <div className="flex flex-row">
+          <div>one</div>
+          <div>one</div>
+          <div>one</div>
+          <div>one</div>
+        </div>
+      </div>
+    </header>
   );
+  // return (
+  //   <Navbar expand="sm" fixed="top">
+  //     <Container fluid>
+  //       <Nav.Link onClick={() => scroll.scrollToTop()}>
+  //         <Brand>F. Ho</Brand>
+  //       </Nav.Link>
+  //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  //       <Navbar.Collapse id="basic-navbar-nav">
+  //         <Nav className="ms-auto align-items-center">
+  //           <Toggle className="me-auto" toggleThemeMode={toggleThemeMode} />
+  //           <Nav.Link
+  //             onClick={() => {
+  //               scroller.scrollTo("featured-works", {
+  //                 duration: 800,
+  //                 delay: 0,
+  //                 smooth: "easeInOutQuart",
+  //                 offset: -150,
+  //               });
+  //             }}
+  //           >
+  //             <Text>Work</Text>
+  //           </Nav.Link>
+  //           <Nav.Link
+  //             onClick={() => {
+  //               scroller.scrollTo("about", {
+  //                 duration: 800,
+  //                 delay: 0,
+  //                 smooth: "easeInOutQuart",
+  //                 offset: -150,
+  //               });
+  //             }}
+  //           >
+  //             <Text>About</Text>
+  //           </Nav.Link>
+  //           <Nav.Link onClick={() => {
+  //               scroller.scrollTo("contact", {
+  //                 duration: 800,
+  //                 delay: 0,
+  //                 smooth: "easeInOutQuart",
+  //                 offset: -150,
+  //               });
+  //             }}>
+  //             <Text>Contact</Text>
+  //           </Nav.Link>
+  //         </Nav>
+  //       </Navbar.Collapse>
+  //     </Container>
+  //   </Navbar>
+  // );
 }
