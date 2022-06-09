d5c0cd4af9011ed4cfab6d7e92089ce43c200e19
otrachea <xtrachea@gmail.com>
2022-06-09 15:39:35 -0400

    More styling for navbar

--- src/App/Header.tsx
+++ src/App/Header.tsx
@@ -1,84 +1,63 @@
+import { useState } from "react";
 import { animateScroll as scroll, scroller } from "react-scroll";
 
+type Mode = "LIGHT" | "DARK";
+
 export default function Header(props: any) {
-  const { toggleThemeMode } = props;
+  const [mode, setMode] = useState<Mode>("DARK");
+
+  const navlinks = [
+    { element: "about", text: "About" },
+    { element: "featured-works", text: "Featured Works" },
+    { element: "contact", text: "Contact" },
+  ];
 
   return (
-    <header className="fixed flex flex-row text-white">
+    <header className="w-100 fixed flex flex-row justify-between text-white">
       <div
-        className="text-8xl font-bold text-orange-500 hover:cursor-pointer dark:text-white"
+        className="text-5xl font-bold text-orange-500 hover:cursor-pointer dark:text-white"
         onClick={() => scroll.scrollToTop()}
       >
         F. Ho
       </div>
       <div>
-        <div className="flex flex-row">
-          <div>one</div>
-          <div>one</div>
-          <div>one</div>
-          <div>one</div>
-        </div>
+        <ul className="flex flex-row">
+          {navlinks.map((link) => {
+            return (
+              <li
+                className="mx-3 text-3xl"
+                onClick={() => {
+                  scroller.scrollTo(link.element, {
+                    duration: 800,
+                    delay: 0,
+                    smooth: "easeInOutQuart",
+                    offset: -150,
+                  });
+                }}
+              >
+                {link.text}
+              </li>
+            );
+          })}
+        </ul>
       </div>
       <button
-        className="border border-black bg-white p-3 text-black"
+        className="border border-black bg-white px-3 py-1 text-xl text-black"
         onClick={() => {
           const html = document.querySelector("html")!;
-          html.classList.contains("dark")
-            ? html.classList.remove("dark")
-            : html.classList.add("dark");
+
+          if (html.classList.contains("dark")) {
+            html.classList.remove("dark");
+            setMode("LIGHT");
+          } else {
+            html.classList.add("dark");
+            setMode("DARK");
+          }
         }}
       >
-        Click
+        {mode === "LIGHT" && "Switch to Dark"}
+        {mode === "DARK" && "Switch to Light"}
       </button>
     </header>
   );
-  // return (
-  //   <Navbar expand="sm" fixed="top">
-  //     <Container fluid>
-  //       <Nav.Link onClick={() => scroll.scrollToTop()}>
-  //         <Brand>F. Ho</Brand>
-  //       </Nav.Link>
-  //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  //       <Navbar.Collapse id="basic-navbar-nav">
-  //         <Nav className="ms-auto align-items-center">
-  //           <Toggle className="me-auto" toggleThemeMode={toggleThemeMode} />
-  //           <Nav.Link
-  //             onClick={() => {
-  //               scroller.scrollTo("featured-works", {
-  //                 duration: 800,
-  //                 delay: 0,
-  //                 smooth: "easeInOutQuart",
-  //                 offset: -150,
-  //               });
-  //             }}
-  //           >
-  //             <Text>Work</Text>
-  //           </Nav.Link>
-  //           <Nav.Link
-  //             onClick={() => {
-  //               scroller.scrollTo("about", {
-  //                 duration: 800,
-  //                 delay: 0,
-  //                 smooth: "easeInOutQuart",
-  //                 offset: -150,
-  //               });
-  //             }}
-  //           >
-  //             <Text>About</Text>
-  //           </Nav.Link>
-  //           <Nav.Link onClick={() => {
-  //               scroller.scrollTo("contact", {
-  //                 duration: 800,
-  //                 delay: 0,
-  //                 smooth: "easeInOutQuart",
-  //                 offset: -150,
-  //               });
-  //             }}>
-  //             <Text>Contact</Text>
-  //           </Nav.Link>
-  //         </Nav>
-  //       </Navbar.Collapse>
-  //     </Container>
-  //   </Navbar>
-  // );
 }
