23b0b17025fd2b7a8bbee4e50e3bb5b30c2a7311
otrachea <xtrachea@gmail.com>
2022-06-09 18:02:51 -0400

    Blinking cursor animation, added custom colours

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -18,52 +18,50 @@ export default function App() {
   const [themeMode, toggleThemeMode] = useDarkMode();
 
   return (
-    <>
-      <Theme themeMode={themeMode}>
-        <div className="bg-slate-50 transition ease-in-out dark:bg-zinc-900">
-          <Header toggleThemeMode={toggleThemeMode} />
-          <div className="h-[100vh]">
-            <h1 className="text-7xl text-slate-900 dark:text-white">
-              Hi, I'm <span className="text-[#3F88C5]">Francesca Ho</span> and
-              I'm a
-              <span className="font-pixel text-[#B3005C]">
-                {" "}
-                Full-Stack Web Developer
-              </span>
-            </h1>
-            <h1 className="text-3xl text-slate-900 dark:text-white">
-              I'm currently looking for work and open for projects!
-            </h1>
-          </div>
-          <Section>
-            <Row>
-              <SubHeader>Featured Works</SubHeader>
-            </Row>
-            <Row>
-              <Element name="featured-works">
-                <FeaturedWork />
-              </Element>
-            </Row>
-          </Section>
-          <Section>
-            <Row>
-              <SubHeader>About</SubHeader>
-            </Row>
-            <Row>
-              <Element name="about">fdsfdsfksd</Element>
-            </Row>
-          </Section>
+    <Theme themeMode={themeMode}>
+      <div className="bg-slate-50 transition ease-in-out dark:bg-zinc-900">
+        <Header toggleThemeMode={toggleThemeMode} />
+        <div className="h-[100vh]">
+          <h1 id="header" className="text-7xl text-slate-900 dark:text-white">
+            Hi, I'm <span className="text-bleu">Francesca Ho</span> and I'm
+            a
+            <span className="font-pixel text-maroon">
+              {" "} Full-Stack Web Developer
+            </span>
+            <span className="inline-block animate-cursor-blink bg-maroon text-transparent text-6xl">{"|"}</span>
+          </h1>
+          <h1 className="text-3xl text-slate-900 dark:text-white">
+            I'm currently looking for work and open for projects!
+          </h1>
+        </div>
+        <Section>
           <Row>
-            <SubHeader>Contact Me!</SubHeader>
+            <SubHeader>Featured Works</SubHeader>
           </Row>
           <Row>
-            <Element name="contact">
-              <Contact />
+            <Element name="featured-works">
+              <FeaturedWork />
             </Element>
           </Row>
-        </div>
-        <Footer />
-      </Theme>
-    </>
+        </Section>
+        <Section>
+          <Row>
+            <SubHeader>About</SubHeader>
+          </Row>
+          <Row>
+            <Element name="about">fdsfdsfksd</Element>
+          </Row>
+        </Section>
+        <Row>
+          <SubHeader>Contact Me!</SubHeader>
+        </Row>
+        <Row>
+          <Element name="contact">
+            <Contact />
+          </Element>
+        </Row>
+      </div>
+      <Footer />
+    </Theme>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
       fontFamily: {
         pixel: ["VT323"],
       },
-    }
+      keyframes: {
+        blink: {
+          "0%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "cursor-blink": "blink 1.3s steps(2) infinite",
+      },
+      colors: {
+        maroon: "#B3005C",
+        bleu: "#3F88C5"
+      }
+    },
   },
   plugins: [],
   darkMode: "class",
