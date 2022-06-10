71f25636db452663718816fb88615a8943164298
otrachea <xtrachea@gmail.com>
2022-06-10 16:29:19 -0400

    Make landing it's own component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,6 +9,7 @@ import Background from "./Background";
 import { ParallaxProvider } from "react-scroll-parallax";
 
 import "./App.css";
+import Landing from "./Landing";
 
 export default function App() {
   return (
@@ -22,24 +23,7 @@ export default function App() {
         <Header />
         <div className="z-10 flex h-[100vh]">
           <div className="z-10 w-[100vw]" id="landing">
-            <div
-              className="z-10 flex h-[100vh] w-[100vw] justify-center text-slate-900 dark:text-white"
-              id="landing"
-            >
-              <div className="mt-[17vh] w-[70vw] text-left">
-                <h1 className="text-1xl text-slate-900 dark:text-white lg:text-7xl lg:leading-relaxed">
-                  Hi, I'm <span className="text-purple-600">Francesca Ho</span>{" "}
-                  and I'm a
-                  <span className="text-maroon"> Full-Stack Web Developer</span>
-                  <span className="inline-block animate-cursor-blink bg-maroon text-transparent">
-                    {"|"}
-                  </span>
-                </h1>
-                <h1 className="font-minecraft text-base text-slate-900 dark:text-white lg:text-3xl">
-                  I'm currently looking for work and open for projects!
-                </h1>
-              </div>
-            </div>
+            <Landing />
           </div>
 
           <div className="z-10 w-[100vw]" id="about">
