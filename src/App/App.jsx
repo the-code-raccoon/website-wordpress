20405115140a7dddd60c50891111d43fc8c747b2
otrachea <xtrachea@gmail.com>
2022-06-10 14:04:01 -0400

    Responsive text on landing page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -19,16 +19,19 @@ export default function App() {
       >
         <Header />
         <div className="flex h-[100vh] items-center">
-          <div className="z-10 flex w-[100vw] justify-center" id="landing">
-            <div className="w-[70%] text-left">
-              <h1 className="text-7xl text-slate-900 dark:text-white">
+          <div
+            className="z-10 flex h-[100vh] w-[100vw] justify-center"
+            id="landing"
+          >
+            <div className="w-[70vw] text-left">
+              <h1 className="text-1xl mt-[17vh] text-slate-900 dark:text-white lg:text-7xl lg:leading-relaxed">
                 Hi, I'm <span className="text-bleu">Francesca Ho</span> and I'm
                 a<span className="text-maroon"> Full-Stack Web Developer</span>
-                <span className="inline-block animate-cursor-blink bg-maroon text-6xl text-transparent">
+                <span className="inline-block animate-cursor-blink bg-maroon text-transparent">
                   {"|"}
                 </span>
               </h1>
-              <h1 className="font-minecraft text-3xl text-slate-900 dark:text-white">
+              <h1 className="font-minecraft text-base text-slate-900 dark:text-white lg:text-3xl">
                 I'm currently looking for work and open for projects!
               </h1>
             </div>
