a5d3aa679d26ce22ab073b05a39105442e98eb26
otrachea <xtrachea@gmail.com>
2022-06-12 22:45:07 -0400

    Finish featured projects (at least desktop ver)

--- src/App/FeaturedWork.tsx
+++ src/App/FeaturedWork.tsx
@@ -2,23 +2,56 @@ export default function FeaturedWork() {
   const works = [
     {
       name: "Charcuterie",
-      link: "https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/Charcuterie.svg",
+      logo: "https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/Charcuterie.svg",
       bgcolor: "#ffffff",
+      description: `An in-restaurant web platform that gives customers a digital menu
+      and ordering interface at their fingertips, just by scanning a QR
+      code with their phone. Restaurants have a dashboard to manage
+      their menu, seated customers and incoming orders.`,
+      image:
+        "https://raw.githubusercontent.com/robotsch/charcuterie/main/docs/orders.png",
+      github:
+        "https://raw.githubusercontent.com/robotsch/charcuterie/main/docs/orders.png",
+      preview: null,
+      stack: `NodeJS, TypeScript, React, MaterialUI, Express, MongoDB, SocketIO`,
+      btntext: "black",
     },
     {
       name: "Foodie",
-      link: "https://raw.githubusercontent.com/the-code-raccoon/website/c0fea90a41bf7b8ad4c447bae694010b830499c7/assets/foodie.svg",
+      logo: "https://raw.githubusercontent.com/the-code-raccoon/website/c0fea90a41bf7b8ad4c447bae694010b830499c7/assets/foodie.svg",
       bgcolor: "#DA4167",
+      description: `A restaurant ordering pickup app where users can order from a menu, edit their cart, be notified via SMS of their order and look at their past orders. Restaurant can mange incoming orders via SMS.`,
+      image:
+        "https://raw.githubusercontent.com/robotsch/Foodie/main/docs/landing-page.png",
+      github: "https://github.com/robotsch/Foodie",
+      preview: "lhl-midterm-foodie.herokuapp.com/",
+      stack: "NodeJS, Express, jQuery, Bootstrap, PostgreSQL",
+      btntext: "black",
     },
     {
       name: "GOTHREAU",
-      link: "https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/GOTHREAU.svg",
+      logo: "https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/GOTHREAU.svg",
       bgcolor: "#674EA7",
+      description: "Portfolio website for the musical artist Gothreau.",
+      image:
+        "https://raw.githubusercontent.com/the-code-raccoon/the-code-raccoon.github.io/main/src/assets/gothreau.png",
+      github: "https://github.com/the-code-raccoon/gothreau",
+      preview: null,
+      stack: "React, TailwindCSS",
+      btntext: "white",
     },
     {
       name: "Interview Scheduler",
-      link: "https://raw.githubusercontent.com/the-code-raccoon/scheduler/main/public/images/logo.png",
+      logo: "https://raw.githubusercontent.com/the-code-raccoon/scheduler/main/public/images/logo.png",
       bgcolor: "#222f3e",
+      description:
+        "Interview Scheduler is a single page application that lets users book, edit and cancel interviews.",
+      image:
+        "https://raw.githubusercontent.com/the-code-raccoon/scheduler/main/docs/landing.png",
+      github: "https://github.com/the-code-raccoon/scheduler",
+      preview: null,
+      stack: "React, Cypress, StoryBook, Jest",
+      btntext: "white",
     },
   ];
 
@@ -29,104 +62,70 @@ export default function FeaturedWork() {
   return (
     <div className="z-10 flex h-[100vh] w-[100%] justify-center text-slate-900">
       <div className="xl-w-[80%] mt-[10vh] flex w-[90%] justify-between text-center">
-        {/* {works.map(({ name, link, bgcolor }) => {
+        {works.map((work) => {
           return (
             <div
-              className={`flex h-[150px] w-[400px] rounded-[20px] bg-[${bgcolor}]`}
+              className="md-w-[30%] mx-3 w-[80%] max-w-[400px]"
+              key={work.name}
             >
-              <img
-                src={link}
-                alt={name}
-                className="m-auto w-[300px] transition-[width] ease-in-out hover:w-[350px]"
-              />
+              <div className="flex h-[75vh] flex-wrap content-between justify-evenly rounded-md bg-slate-200/[0.8] pt-4 pb-8">
+                <div className="w-[100%]">
+                  <div
+                    className={`flex h-[100px] w-[350px] rounded-[10px] bg-[${work.bgcolor}] mx-auto mb-4`}
+                  >
+                    <img
+                      src={work.logo}
+                      alt={work.name}
+                      className="m-auto w-[280px] transition-[width] ease-in-out hover:w-[300px]"
+                    />
+                  </div>
+
+                  <img
+                    src={work.image}
+                    alt={work.name}
+                    className="mx-auto w-[350px]"
+                  />
+                </div>
+
+                <div className="w-[90%]">
+                  <h1 className="text-sm">{work.description}</h1>
+                </div>
+
+                <div className="w-[90%]">
+                  <h1 className="text-sm">Made with: {work.stack}.</h1>
+                </div>
+
+                {work.preview && (
+                  <a
+                    href="https://google.com"
+                    className="text-black no-underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button
+                      className={`bg-[${work.bgcolor}] text-${work.btntext} delay-80 h-[45px] w-[150px] rounded-md pb-1 text-center shadow transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-purple-300`}
+                    >
+                      View Live
+                    </button>
+                  </a>
+                )}
+
+                <a
+                  href={work.github}
+                  className="text-black no-underline"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    className={`bg-[${work.bgcolor}] text-${work.btntext} delay-80 h-[45px] w-[150px] rounded-md pb-1 text-center shadow transition duration-300 ease-in-out hover:-translate-y-1 hover:bg-purple-300`}
+                  >
+                    GitHub Page
+                  </button>
+                </a>
+              </div>
             </div>
           );
-        })} */}
-        <div className="md-w-[30%] mx-3 w-[80%] max-w-[400px]">
-          <div className="flex h-[75vh] flex-wrap content-evenly justify-evenly rounded-md bg-slate-200/[0.5] p-3">
-            <div
-              className={`flex h-[150px] w-[400px] rounded-[20px] bg-[${"#ffffff"}]`}
-            >
-              <img
-                src="https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/Charcuterie.svg"
-                alt={"Charcuterie"}
-                className="m-auto w-[280px] transition-[width] ease-in-out hover:w-[320px]"
-              />
-            </div>
-            <h1 className="text-sm">
-              An in-restaurant web platform that gives customers a digital menu
-              and ordering interface at their fingertips, just by scanning a QR
-              code with their phone. Restaurants have a dashboard to manage
-              their menu, seated customers and incoming orders.
-            </h1>
-            <a
-              href="https://google.com"
-              className="text-black no-underline"
-              target="_blank"
-              rel='noreferrer'
-            >
-              <button
-                className={`bg-[${"#ffffff"}] h-[45px] w-[150px] rounded-md pb-1 text-center shadow transition-fontSize ease-in-out hover:text-lg`}
-              >
-                View Live
-              </button>
-            </a>
-            <a
-              href="https://google.com"
-              className="text-black no-underline"
-              target="_blank"
-              rel='noreferrer'
-            >
-              <button
-                className={`bg-[${"#ffffff"}] h-[45px] w-[150px] rounded-md pb-1 text-center shadow transition-fontSize ease-in-out hover:text-lg`}
-              >
-                GitHub Page
-              </button>
-            </a>
-          </div>
-        </div>
-
-        <div className="md-w-[30%] mx-3 w-[80%] max-w-[400px]">
-          <div className="flex h-[75vh] flex-wrap content-evenly justify-evenly rounded-md bg-slate-200/[0.5] p-3">
-            <div
-              className={`flex h-[150px] w-[400px] rounded-[20px] bg-[${"#ffffff"}]`}
-            >
-              <img
-                src="https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/Charcuterie.svg"
-                alt={"Charcuterie"}
-                className="m-auto w-[300px] transition-[width] ease-in-out hover:w-[350px]"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="md-w-[30%] mx-3 w-[80%] max-w-[400px]">
-          <div className="flex h-[75vh] flex-wrap content-evenly justify-evenly rounded-md bg-slate-200/[0.5] p-3">
-            <div
-              className={`flex h-[150px] w-[400px] rounded-[20px] bg-[${"#ffffff"}]`}
-            >
-              <img
-                src="https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/Charcuterie.svg"
-                alt={"Charcuterie"}
-                className="m-auto w-[300px] transition-[width] ease-in-out hover:w-[350px]"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="md-w-[30%] mx-3 w-[80%] max-w-[400px]">
-          <div className="flex h-[75vh] flex-wrap content-evenly justify-evenly rounded-md bg-slate-200/[0.5] p-3">
-            <div
-              className={`flex h-[150px] w-[400px] rounded-[20px] bg-[${"#ffffff"}]`}
-            >
-              <img
-                src="https://raw.githubusercontent.com/the-code-raccoon/website/ebca3ba315deb25ed16b66ab95e19bb07f4b6ac2/assets/Charcuterie.svg"
-                alt={"Charcuterie"}
-                className="m-auto w-[300px] transition-[width] ease-in-out hover:w-[350px]"
-              />
-            </div>
-          </div>
-        </div>
+        })}
       </div>
     </div>
   );
